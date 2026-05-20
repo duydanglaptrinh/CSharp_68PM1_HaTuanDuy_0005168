aba9159f5d7b4442a07dbc21cf2bf6bd2760e278
Hà Tuấn Duy <HaBopChit1996@gmail.com>
2026-05-20 16:05:35 +0700

    feat add UserControl + MenuStrip

--- WindowsFormsApp01/Main.cs
+++ WindowsFormsApp01/Main.cs
@@ -1,48 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp01
 {
     public partial class Main : Form
     {
+        // Khởi tạo sẵn 2 UserControl
+        private UC_QLLH uc_qllh = new UC_QLLH();
+        private UC_QLSV uc_qlsv = new UC_QLSV();
+
         public Main()
         {
+            // Lệnh này bắt buộc phải gọi để nó nạp đoạn code trong Designer.cs lên màn hình
             InitializeComponent();
         }
 
-        private void btn_login_Click(object sender, EventArgs e)
+        private void Main_Load(object sender, EventArgs e)
         {
+            // Vừa mở lên thì nạp ngay màn hình sinh viên
+            ShowUserControl(uc_qlsv);
+        }
 
-            String username = txt_username.Text;
-            String password = txt_password.Text;
+        // --- HÀM XỬ LÝ CHUYỂN TRANG ---
+        private void ShowUserControl(UserControl uc)
+        {
+            panelContainer.Controls.Clear();
+            uc.Dock = DockStyle.Fill;
+            panelContainer.Controls.Add(uc);
+        }
 
-            if (username == "0003168@st.huce.edu.vn" && password == "0003168")
+        private void btnQLLH_Click(object sender, EventArgs e)
+        {
+            ShowUserControl(uc_qllh);
+            DoiTrangThaiNut(btnQLLH);
+        }
+
+        private void btnQLSV_Click(object sender, EventArgs e)
+        {
+            ShowUserControl(uc_qlsv);
+            DoiTrangThaiNut(btnQLSV);
+        }
+
+        private void btnDangXuat_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc chắn muốn đăng xuất?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
             {
-                MessageBox.Show("Đăng nhập thành công");
-                Form QLSinhVien  = new QLSinhVien();
-
-                // 2. Hien thi Form moi
-                QLSinhVien.Show();
-
-                // 3. An Form hien tai (Form Quan ly Sinh vien)
-                this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("Đăng nhập thất bại");
+                this.Close();
             }
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        // Đổi Font chữ bôi đậm
+        private void DoiTrangThaiNut(Button activeBtn)
         {
+            btnQLSV.Font = new Font("Segoe UI", 10F, FontStyle.Regular);
+            btnQLLH.Font = new Font("Segoe UI", 10F, FontStyle.Regular);
 
+            activeBtn.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
         }
     }
 }
